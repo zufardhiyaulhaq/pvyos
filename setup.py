f6270af7fa9ebdc0973350eb5a3db0db95d26d65
zufardhiyaulhaq <zufar@ieee.org>
2017-12-06 13:59:07 +0700

    v0.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 setup(
   name = 'pvyos',
   packages = ['pvyos'], 
-  version = '0.0.1',
+  version = '0.1',
   description = ' Pvyos is a simple library build on pexpect to connect and configure Vyos OS.',
   author = 'Zufar Dhiyaulhaq',
   author_email = 'zufar@ieee.org',
   url = 'https://github.com/zufardhiyaulhaq/pvyos',
-  download_url = 'https://github.com/zufardhiyaulhaq/pvyos/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/zufardhiyaulhaq/pvyos/archive/0.1.tar.gz',
   keywords = ['vyos', 'networking', 'automation'],
 )
